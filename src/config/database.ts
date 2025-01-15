a2087de41c660b3ecbe811de6611cedfc8cb1c2d
niigyanchristian <niigyanchristian@gmail.com>
2025-01-15 13:16:35 +0000

    configure database

---
+++ src/config/database.ts
@@ -0,0 +1,15 @@
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize(process.env.DATABASE_URL as string, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false, // Adjust based on your security needs
+        },
+    },
+    logging: console.log,
+    // logging: true,
+});
+
+export default sequelize;
