7f4423d5c708b08efe7653b62a3d23b2a41d78ca
niigyanchristian <niigyanchristian@gmail.com>
2025-01-21 22:58:07 +0000

    fix auth issues

--- src/routes/teams.ts
+++ src/routes/teams.ts
@@ -3,11 +3,12 @@ import express from 'express';
 import { validateRequestBody } from '../middleware/validate';
 import { createTeamRequest, deleteTeamRequest, getTeamByIdRequest, getTeamsRequest, updateTeamByIdRequest } from '../controllers/team';
 import { createTeamValidator, teamByIdValidator } from '../constants/validators';
+import { authorizeAdmin } from '../middleware/auth';
 
 const router = express.Router();
 
 
-router.post('/', createTeamValidator, validateRequestBody, createTeamRequest);
+router.post('/', createTeamValidator, validateRequestBody, authorizeAdmin(), createTeamRequest);
 
 router.get('/', getTeamsRequest);
 //I don't get this, there is an error in validateRequestBody 
@@ -15,8 +16,8 @@ router.get('/', getTeamsRequest);
 
 router.get('/:id', teamByIdValidator, validateRequestBody, getTeamByIdRequest);
 
-router.put('/:id', teamByIdValidator, validateRequestBody, updateTeamByIdRequest);
+router.put('/:id', teamByIdValidator, validateRequestBody, authorizeAdmin(), updateTeamByIdRequest);
 
-router.delete('/:id', teamByIdValidator, validateRequestBody, deleteTeamRequest);
+router.delete('/:id', teamByIdValidator, validateRequestBody, authorizeAdmin(), deleteTeamRequest);
 
 export default router;
